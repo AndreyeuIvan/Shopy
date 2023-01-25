2e18ced10bbf7a5e52ebdc7fb2df549cc4a1f318
ivan <andreeviv@icloud.com>
2023-01-25 15:35:46 +0100

    serializers created, views applied

--- shopy/models.py
+++ shopy/models.py
@@ -1,28 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.mail import send_mail
 from django.urls import reverse
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-from phone_field import PhoneField
-
-
-class User(AbstractUser):
-    email = models.EmailField('email address', unique=True)
-    first_name = models.CharField('first name', max_length=30, blank=True)
-    last_name = models.CharField('last name', max_length=30, blank=True)
-    is_active = models.BooleanField('active', default=True)
-    phone_number = PhoneField(blank=True, help_text='Contact phone number')
-
-    class Meta:
-        verbose_name = ('User')
-        verbose_name_plural = ('Users')
-
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        '''
-        Sends an email to this User.
-        '''
-        send_mail(subject, message, from_email, [self.email], **kwargs)
+from my_auth.models import User
 
 
 class Account(models.Model):
@@ -69,7 +49,7 @@ class Product(models.Model):
 
 class Reserved(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    product_id = models.ForeignKey("Product", on_delete=models.CASCADE)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
     number_of_units = models.IntegerField(
         default=1,
         validators=[MaxValueValidator(1000), MinValueValidator(1)]
@@ -80,7 +60,7 @@ class Reserved(models.Model):
         verbose_name_plural = ("Reserveds")
 
     def __str__(self):
-        return f'{self.user}_{self.product_id.name}'
+        return f'{self.user}_{self.product.name}'
 
     def get_absolute_url(self):
         return reverse("Reserved_detail", kwargs={"pk": self.pk})
--- shopy/views.py
+++ shopy/views.py
@@ -4,8 +4,10 @@ from rest_framework import mixins
 from rest_framework import permissions
 from rest_framework import viewsets
 from rest_framework.views import APIView
+from rest_framework import response
+from rest_framework import status
 
-from shopy.serializers import ReserverSerializer
+from shopy.serializers import ReserverSerializer, AccountSerializer, ProductSerializer
 from shopy.models import Reserved, Account, Product
 
 
@@ -22,12 +24,36 @@ class ReversedViewSet(viewsets.ReadOnlyModelViewSet):
         return Reserved.objects.filter(user=self.request.user.id)
 
 
+class AccountReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
+
+    serializer_class = AccountSerializer
+    queryset = Account.objects.all()
+
+
+class ProductReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
+
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
 class BasketView(APIView):
     #http_method_names = ['POST', 'PUT', 'DELETE']
     
     def post(self, request):
+        """
+        {"product_id": 1,
+        "number_of_units":1}
+        """
+        data = {
+            "product": 1,
+            "number_of_units":1
+        }
         import pdb;pdb.set_trace()
-        pass
+        serializer = ReserverSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def put(self, request):
         pass
